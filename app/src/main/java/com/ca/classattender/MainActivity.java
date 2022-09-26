4d3b678a48c7c0b5ba4e721785aab29a8d5ea556
jigne <jigneshrathod154@gmail.com>
2022-09-26 17:11:21 +0530

    Firebase connection Commit

--- app/src/main/java/com/ca/classattender/MainActivity.java
+++ app/src/main/java/com/ca/classattender/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    // Firebase Auth chal raha hai
+    // Realtime db bhi chal raha hai
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
