22dd329a100f824e202738a7d233111939001e7b
jigne <jigneshrathod154@gmail.com>
2022-10-03 09:59:17 +0530

    Data retrieval from db of slot

---
+++ app/src/main/java/com/ca/classattender/FbData.java
@@ -0,0 +1,21 @@
+package com.ca.classattender;
+
+public class FbData {
+    public int template;
+    public String subject;
+    public String subcode;
+    public String subteacher;
+    public String subtime;
+
+    public FbData(){
+
+    }
+
+    public FbData(int template, String subject, String subcode, String subteacher, String subtime){
+        this.template = template;
+        this.subject = subject;
+        this.subcode = subcode;
+        this.subteacher = subteacher;
+        this.subtime = subtime;
+    }
+}
--- app/src/main/java/com/ca/classattender/MainActivity.java
+++ app/src/main/java/com/ca/classattender/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ca.classattender;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,11 +21,16 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<arr> extends AppCompatActivity {
 
     FloatingActionButton fabAddSlot;
     ImageButton imgClose, imgAddSlot;
@@ -32,12 +38,17 @@ public class MainActivity extends AppCompatActivity {
     TextView tvShowTime, tvError;
     Spinner spDay, spSubject;
     int hr, min;
+    int dayIndex;
     String strTime="";
 //    RecyclerView rvMondaySlots, rvTuesdaySlots, rvWednesdaySlots, rvThursdaySlots, rvFridaySlots, rvSaturdaySlots;
     RecyclerView rvDaysSlots[] = new RecyclerView[6];
     ArrayList<ArrayList<SlotModel>> slotList = new ArrayList<>();
     ArrayList<Integer> slotTemplateImg = new ArrayList<>();
     SlotRvAdapter slotRvAdapters[] = new SlotRvAdapter[6];
+
+//    ArrayList<String> subAl = new ArrayList<>();
+    DatabaseReference dbRefList = FirebaseDatabase.getInstance().getReference("class_attender/otps/it");
+    ArrayList<String> slotDaysList = new ArrayList<>();
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -60,6 +71,18 @@ public class MainActivity extends AppCompatActivity {
         slotTemplateImg.add(R.drawable.slot_template_5);
         slotTemplateImg.add(R.drawable.slot_template_6);
 
+        slotDaysList.add("mon");
+        slotDaysList.add("tue");
+        slotDaysList.add("wed");
+        slotDaysList.add("thu");
+        slotDaysList.add("fri");
+        slotDaysList.add("sat");
+
+//        subAl.add("DS");
+//        subAl.add("ETC");
+//        subAl.add("DF");
+//        subAl.add("PAS");
+
         try {
             for (int i=0; i<6; i++){
                 rvDaysSlots[i].setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false));
@@ -70,6 +93,19 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
         }
+
+//        try {
+//            for (String day: slotDaysList){
+//                for (int i=1; i<=3; i++){
+//                    dbRefList.child(day).child("slot"+i).child("subject").setValue(day + " " + subAl.get(i-1));
+//                    dbRefList.child(day).child("slot"+i).child("subcode").setValue("3130004");
+//                    dbRefList.child(day).child("slot"+i).child("subteacher").setValue("MNP");
+//                    dbRefList.child(day).child("slot"+i).child("subtime").setValue("10:30");
+//                }
+//            }
+//        } catch (Exception e) {
+//            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+//        }
 
         fabAddSlot.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -110,8 +146,16 @@ public class MainActivity extends AppCompatActivity {
                         }else if(strTime.equals("")){
                             tvError.setText("Set time!");
                         }else {
-                            slotList.get(positionOfDay-1).add(new SlotModel(slotTemplateImg.get((positionOfSubject-1)%6), sub, strTime, 3130004, "MNP"));
-                            slotRvAdapters[positionOfDay-1].notifyDataSetChanged();
+//                            slotList.get(positionOfDay-1).add(new SlotModel(slotTemplateImg.get((positionOfSubject-1)%6), sub, strTime, 3130004, "MNP"));
+//                            slotRvAdapters[positionOfDay-1].notifyDataSetChanged();
+
+                            dbRefList.child(slotDaysList.get(positionOfDay-1)).child("slot"+slotList.get(positionOfDay-1).size()+1).child("template").setValue((positionOfSubject-1)%6);
+                            dbRefList.child(slotDaysList.get(positionOfDay-1)).child("slot"+slotList.get(positionOfDay-1).size()+1).child("subject").setValue(sub);
+                            dbRefList.child(slotDaysList.get(positionOfDay-1)).child("slot"+slotList.get(positionOfDay-1).size()+1).child("subcode").setValue("3130004");
+                            dbRefList.child(slotDaysList.get(positionOfDay-1)).child("slot"+slotList.get(positionOfDay-1).size()+1).child("subteacher").setValue("MNP");
+                            dbRefList.child(slotDaysList.get(positionOfDay-1)).child("slot"+slotList.get(positionOfDay-1).size()+1).child("subtime").setValue(strTime);
+
+                            getAllSlots();
                             bsAddSlot.dismiss();
                         }
                     }
@@ -142,5 +186,43 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    public void getAllSlots(){
+        Toast.makeText(this, "In getAllSlots", Toast.LENGTH_SHORT).show();
+        try {
+            slotList.clear();
+            for (dayIndex=0; dayIndex<slotDaysList.size(); dayIndex++){
+                slotList.add(new ArrayList<>());
+                dbRefList.child(slotDaysList.get(dayIndex)).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        try {
+                            FbData fbData = snapshot.child("slot1").getValue(FbData.class);
+                            slotList.get(dayIndex).add(new SlotModel(slotTemplateImg.get(fbData.template), fbData.subject, fbData.subtime, fbData.subcode, fbData.subteacher));
+                            Toast.makeText(MainActivity.this, "after adding slot", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, fbData.subject+fbData.subcode+fbData.subteacher+fbData.subtime, Toast.LENGTH_SHORT).show();
+                            int i = 2;
+                            while (fbData.subject != null){
+                                fbData = snapshot.child("slot"+i).getValue(FbData.class);
+                                slotList.get(dayIndex).add(new SlotModel(fbData.template, fbData.subject, fbData.subtime, fbData.subcode, fbData.subteacher));
+                                Toast.makeText(MainActivity.this, fbData.subject+fbData.subcode+fbData.subteacher+fbData.subtime, Toast.LENGTH_SHORT).show();
+                                i++;
+                            }
+                            slotRvAdapters[dayIndex].notifyDataSetChanged();
+                        } catch (Exception e) {
+                            Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
